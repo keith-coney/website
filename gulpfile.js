2fff07ad160390c66c5aae7757ccfa4b2b46a20c
Luc Baissac <Luc@MacBook-Pro.lan>
2021-01-29 07:45:48 +1000

    website content and design

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,48 @@
 var gulp = require('gulp');
+const browsersync = require('browser-sync').create();
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
+
+sass.compiler =require('node-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
+var concat = require('gulp-concat');
+var uglifycss = require('gulp-uglifycss');
 
-sass.compiler = require('node-sass');
+gulp.task('sass',
+function () {
+return (
+gulp
+.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css',
+'./sass/**/*.scss'])
+// .pipe(sourcemaps.init())
+.pipe(sass.sync().on('error',
+sass.logError))
+.pipe(sourcemaps.write())
+.pipe(concat('style.css'))
+.pipe(
+uglifycss({
+maxLineLen:
+80,
+uglyComments:
+true,
+})
+)
+.pipe(gulp.dest('./'))
+);
 
-gulp.task('sass', function() {
-  return gulp
-    .src([
-      './node_modules/bootstrap/dist/css/bootstrap.min.css',
-      './sass/**/*.scss'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('scripts', function() {
-  return gulp
-    .src([
-      './node_modules/jquery/dist/jquery.js',
-      './node_modules/bootstrap/dist/js/bootstrap.min.js',
-      './node_modules/scrollreveal/dist/scrollreveal.min.js', 
-      './js/custom.js',
-    ])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./js/'));
-});
-
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-
-  // gulp.watch("*.html").on("change", reload);
 });
 
 
-//watch
-gulp.task('watch', ['scripts', 'sass','serve'], function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./js/*.js', ['scripts']);
-  gulp.watch("*.html").on("change", reload);
+
+gulp.task('watch',
+function () {
+
+gulp.watch('./sass/*.scss',
+gulp.series('sass'));
+
 });
+
+
+
+gulp.task('default',
+gulp.series('sass',
+'watch'));
